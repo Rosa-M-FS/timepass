a774aec5d5d775163ac49a80cfb75e5b7cb344f1
Rosa-M-FS <rmlopezrobles@gmail.com>
2025-01-09 21:04:55 +0100

    Ejercicio Terminado

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ const router=express.Router();
 /* const horaMiddleware=require('./middlewares/horaMiddleware'); */
 
 router.get('/',(req,res)=>{
-    
+    const mensaje=req.query.mensaje || '';
     res.send(`<h1>Bienvenido</h1>
         <p>Ahora son las ${req.dateType} </p>
+        <p>${mensaje} </p>
         <a href="/endroute">
         <button >Entrar</button>
         </a>`)
